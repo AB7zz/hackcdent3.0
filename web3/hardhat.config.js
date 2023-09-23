e98cb843bf3d906dd021c892b6e5abe3102e597a
AB7zz <abhinavcv007@gmail.com>
2023-09-23 13:21:59 +0530

    merged everything

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,32 +1,20 @@
 require("@matterlabs/hardhat-zksync-solc");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
+  defaultNetwork: "mumbai",
   networks: {
     hardhat: {},
     mumbai: {
-      url: 'https://rpc.ankr.com/polygon_mumbai'
+      url: 'https://rpc.ankr.com/polygon_mumbai',
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 80001
     },
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 5,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
+    goerli: {
+      url: 'https://goerli.rpc.thirdweb.com/15065ae3c21e0bff07eaf80b713a6ef0',
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 5
     },
   },
   paths: {
