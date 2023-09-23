9f187c6a84b40c63472238c8831f3ccc651a0da0
Jishnu M M <jishnumm4312@gmail.com>
2023-09-23 09:41:21 +0530

    added sidebar components and few features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import { Route,Routes } from 'react-router-dom'
 import Viewaccident from './pages/Viewaccident'
 import Insurance from './pages/Insurance'
 import Submit from './pages/Submit'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div >
+      <Sidebar>
       <Routes>
         <Route path='/' element={<Viewaccident/>}/>
         <Route path='/insurance' element={<Insurance/>}/>
         <Route path='/submit' element={<Submit/>}/>
       </Routes>
+      </Sidebar>
     </div>
   )
 }
